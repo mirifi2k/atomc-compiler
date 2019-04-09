90f923856cbe8f53a8cfd7610b041d7dd90d8f73
mirifi2k <46975339+mirifi2k@users.noreply.github.com>
2019-04-09 17:16:39 +0300

    Update common.h

--- common.h
+++ common.h
@@ -1,6 +1,8 @@
 #ifndef _COMMON_CODE_INCLUDE
 	#define _COMMON_CODE_INCLUDE
 	#define DEBUG_MODE
+	
+	#define SAFEALLOC(var,Type,count) if((var = (Type *) malloc(count * sizeof(Type))) == NULL) err("not enough memory")
 
 	enum
 	{
@@ -11,6 +13,48 @@
 		ASSIGN, EQUAL, NOTEQ, AND, OR, NOT, LESS, LESSEQ, GREATER, GREATEREQ,
 		BREAK, CHAR, VOID, INT, FOR, WHILE, RETURN, STRUCT, DOUBLE, IF, ELSE
 	};
+	enum
+	{
+		TB_INT, TB_DOUBLE, TB_CHAR, TB_STRUCT, TB_VOID
+	};
+	enum
+	{
+		CLS_VAR, CLS_FUNC, CLS_EXTFUNC, CLS_STRUCT
+	};
+	enum
+	{
+		MEM_GLOBAL, MEM_ARG, MEM_LOCAL
+	};
+	
+	typedef struct _Symbol Symbol;
+	
+	typedef struct
+	{
+		int typeBase;
+		Symbol *s;
+		int nElements;
+	} Type;
+	
+	typedef struct
+	{
+		Symbol **begin;
+		Symbol **end;
+		Symbol **after;
+	} Symbols;
+	
+	typedef struct _Symbol
+	{
+		const char *name;
+		int cls;
+		int mem;
+		Type type;
+		int depth;
+		union
+		{
+			Symbols args;
+			Symbols members;
+		};
+	} Symbol;
 
 	typedef struct Token
 	{
@@ -27,8 +71,12 @@
 	} Token;
 
 	int line = 1;
+	int crtDepth = 0;
+	
 	Token *h = NULL;
 	Token *t = NULL;
+	Token *consumedToken;
+	Symbols symbols;
 
 	void err(const char *fmt, ...)
 	{
@@ -99,4 +147,49 @@
 		return NULL;
 	}
 
+	void initSymbols(Symbols *symbols)
+	{
+		symbols->begin = symbols->end = symbols->after = NULL;
+	}
+	
+	Symbol *addSymbol(Symbols *symbols, const char *name, int cls)
+	{
+		Symbol *s;
+		
+		if (symbols->end == symbols->after)
+		{
+			int count = symbols->after - symbols->begin;
+			int n = count * 2;
+			
+			if (!n)
+				n = 1;
+				
+			if ((symbols->begin = (Symbol **) realloc(symbols->begin, n * sizeof(Symbol *))) == NULL)
+				err("malloc() error.");
+				
+			symbols->end = symbols->begin + count;
+			symbols->after = symbols->begin + n;
+		}
+		
+		SAFEALLOC(s, Symbol, 1);
+		*symbols->end++ = s;
+		s->name = name;
+		s->cls = cls;
+		s->depth = crtDepth;
+		return s;
+	}
+	
+	Symbol *findSymbol(Symbols *symbols, const char *name)
+	{
+		int n = symbols->end - symbols->begin;
+		int i;
+		
+		for (i = n - 1; i >= 0; i--)
+		{
+			if (symbols->begin[i]->name == name)
+				return symbols->begin[i];
+		}
+		
+		return NULL;
+	}
 #endif
